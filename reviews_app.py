3136371135cf160f3834497269e88c33ba498cc3
t-alland <tim.r.alland@gmail.com>
2024-05-07 14:02:47 -0400

    Added streamlit app and script to perform summary tasks.

---
+++ reviews_app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import scraper
+import summaryllm
+
+model = "meta/meta-llama-3-70b-instruct"
+
+tab1, tab2 = st.tabs(['Home', "More Info"])
+
+with tab1:
+    st.title("Amazon Reviews Summarizer")
+    st.write("🚀 Instead of scouring reviews yourself, let this LLM pipeline do it for you!")
+
+    instructions = "Instructions:\n 1. Once you're on a product's website on Amazon, scroll down to where you see the reviews. \n 2. Filter the reviews by clicking on the 1, 2, 3, 4, or 5 star reviews (seen on the left hand side of the Amazon webpage).\n 3. Copy this url and paste it into the box below to get your summary!"
+
+    st.write(instructions)
+
+    # Input control for filtering by partial name
+    url = st.text_input("Copy and paste the url here", "")
+
+    reviews = scraper.scrape(url)
+    summary = summaryllm.get_summary(reviews, model)
+    summary
+
+with tab2:
+    "# More Info"
+
+    "The LLM being employed is Meta's llama-3-70b-instruct. This LLM pipeline works by scraping reviews from the Amazon url and passing them to the LLM to distill each one down to a principal complaint or praise. Then, it passes these distilled versions back into the LLM again where it consolidates similar reviews and provides an overarching summary."
---
+++ summaryllm.py
@@ -0,0 +1,49 @@
+from replicate.client import Client
+import os
+
+replicate = Client(api_token=os.environ['REPLICATE_API_TOKEN'])
+
+# Takes a series of reviews and a model name
+def get_summary(reviews, model):
+    init_sum = []
+    for review in reviews:
+    # The meta/meta-llama-3-70b-instruct model can stream output as it's running.
+        msg = ""
+        for event in replicate.stream(
+            model,
+            input={
+                "top_k": 50,
+                "top_p": 0.9,
+                "prompt": f"Here is a product review: {review}\n The principal like or disklike in the review is",
+                "max_tokens": 512,
+                "min_tokens": 10,
+                "temperature": 0.6,
+                "prompt_template": "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou summarize reviews by returning the principal complaint or praise.<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
+                "presence_penalty": 1.15,
+                "frequency_penalty": 0.2
+            },
+        ):
+            msg+=str(event)
+        init_sum.append(msg)
+
+
+    reworded_revs = ""
+    for rev in init_sum:
+        reworded_revs += f"Review: {rev}\n"
+    msg = ""
+    for event in replicate.stream(
+        model,
+        input={
+            "top_k": 50,
+            "top_p": 0.9,
+            "prompt": f"Consider these reviews:\n{reworded_revs} By combining similar reviews, we see that ",
+            "max_tokens": 512,
+            "min_tokens": 0,
+            "temperature": 0.6,
+            "prompt_template": "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou return text in a markdown format. You summarize reviews by consolidating similar reviews, returning the most common reviews first, and sharing how many times that review appeared. Each review must be returned either by itself or consolidated with others. The total number of reviews in the summary should be equal to the number of reviews you receive. Don't reference specific reviews. Don't write anything after the bulleted summary. Your goal is to be helpful to someone who wants to know a product's potential issues. You start by saying: 'Here is a summary of the reviews:'<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
+            "presence_penalty": 1.15,
+            "frequency_penalty": 0.2
+        },
+    ):
+        msg+=str(event)
+    return msg
