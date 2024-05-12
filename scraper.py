68e175a5bfdd9a8da0b873f3dd47a6e226af56af
t-alland <tim.r.alland@gmail.com>
2024-05-12 14:39:51 -0400

    Reverted scraper to previous version. Changed app to only run locally.

--- scraper.py
+++ scraper.py
@@ -1,13 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
 
 # Example:
 # scrape("https://www.amazon.com/product-reviews/B0C2YYBLC1/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&filterByStar=one_star&reviewerType=all_reviews&pageNumber=1#reviews-filter-bar")
 def scrape (url):
     '''Scrape the review texts from the URL, which is expected to be a link to an amazon 1/5 start review page.'''
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(options=chrome_options)
+    firefox_options = Options()
+    firefox_options.add_argument("--headless")
+    driver = webdriver.Firefox(options=firefox_options)
     driver.get(url)
     html = driver.execute_script("return document.documentElement.outerHTML;")
     driver.quit()
